48cf3f114535c873c29c41da5ba26d9db3a38379
petpeevephobia <qamaria.mdsah@gmail.com>
2026-07-16 21:42:27 +0200

    add: vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 // src/app/layout.js
 import "./globals.css"; // Ensure this line is at the top!
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }) {
   return (
